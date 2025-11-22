28d7451f98545acbc68552995d02764b5b664365
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-22 11:37:02 -0500

    commit-script_contador_registro.  : .

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,7 @@ namespace ContadorApp
         public Form1()
         {
             InitializeComponent();
+            // inicializar controles    
         }
 
         private void Form1_Load(object sender, EventArgs e)
